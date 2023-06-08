cc3dfbf15525afa03caceef64227d238498c2b51
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-06-08 22:18:14 +0200

    logout if the user cookie is not set

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -7,7 +7,7 @@ export async function handle({
 
   const theme = JSON.parse(user)?.data?.theme || 'dark';
 
-  if (event.url.pathname.startsWith('/admin') && !JSON.parse(access)?.data?.token) {
+  if (event.url.pathname.startsWith('/admin') && (!JSON.parse(access)?.data?.token || !JSON.parse(user)?.data)) {
     return new Response('Redirect', {
       status: 303,
       headers: { Location: '/login' },
