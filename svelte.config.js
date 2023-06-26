773996b18f7940dc9895e5624385af5dfd678872
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-06-26 19:41:43 +0200

    Revert "allow ts"
    This reverts commit b2f58499303ffeffcaa2abb440810016538f4de9.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
+
+  preprocess: [
+    preprocess({
+      scss: {
+        //	prependData: '@use "src/variables.scss" as *;'
+      },
+    }),
+  ],
 };
 
 export default config;
