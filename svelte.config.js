fa31938fe03d2e61df73222bd859aa3da10db3aa
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-06-26 19:44:29 +0200

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,11 @@ import adapter from '@sveltejs/adapter-node';
 
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
 
   preprocess: [
     preprocess({
-      scss: {
-        //	prependData: '@use "src/variables.scss" as *;'
-      },
     }),
   ],
 };
