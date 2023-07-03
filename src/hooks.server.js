da547565ccd26eda31eb2e5de71c9f589d734ef4
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-07-03 13:10:39 +0200

    add security headers

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -17,7 +17,17 @@ export async function handle({
     throw redirect(302, '/login');
   }
 
-  return resolve(event, {
+  const response = await resolve(event, {
     transformPageChunk: ({ html }) => html.replace('data-bs-theme="auto"', `data-bs-theme="${theme}"`),
   });
+
+  return {
+    ...response,
+    headers: {
+      ...response.headers,
+      'X-Content-Type-Options': 'nosniff',
+      'X-Frame-Options': 'DENY',
+      'X-XSS-Protection': '1; mode=block',
+    },
+  };
 }
