2f0d68cf54533a29766155382406a078a8c43d8e
Mathias Reker <26626066+MathiasReker@users.noreply.github.com>
2023-07-08 15:23:47 +0200

    testing

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -5,17 +5,16 @@ export async function handle({
   resolve,
 }) {
   const user = event.cookies.get('user') ?? '{}';
-  const access = event.cookies.get('access') ?? '{}';
-
-  const theme = JSON.parse(user)?.data?.theme || 'dark';
-
   if (user === '{}') {
     event.cookies.delete('access');
   }
 
+  const access = event.cookies.get('access') ?? '{}';
   if (event.url.pathname.startsWith('/admin') && (!JSON.parse(access)?.data?.token || !JSON.parse(user)?.data)) {
-    throw redirect(302, '/login');
+   // throw redirect(302, '/login');
   }
+
+  const theme = JSON.parse(user)?.data?.theme || 'dark';
 
   return resolve(event, {
     transformPageChunk: ({ html }) => html.replace('data-bs-theme="auto"', `data-bs-theme="${theme}"`),
